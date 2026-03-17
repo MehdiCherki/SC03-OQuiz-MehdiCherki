e2acb60fdafefde40d4ff71f0572d24ea11d206d
BenoitOClock <benoit@oclock.io>
2026-03-17 16:03:01 +0100

    fixed extra request logs (favicon...)

--- api/src/middlewares/log-request.middleware.ts
+++ api/src/middlewares/log-request.middleware.ts
@@ -6,13 +6,12 @@ export function logRequest(req: Request, res: Response, next: NextFunction) {
   const id = req.get("x-request-id") || randomUUID();
   req.requestId = id;
   res.setHeader("x-request-id", id);
-  req.log = logger; //.child({ requestId: id });
+  req.log = logger.child({ requestId: id });
   const start = process.hrtime.bigint();
 
-  console.log("logRequest");
-
   res.on("finish", () => {
-    console.log("logRequest finish");
+    if (!req.originalUrl.startsWith("/api") && req.originalUrl !== "/info")
+      return;
 
     const duration = Number(process.hrtime.bigint() - start) / 1e6; //1ms = 1000 µs = 1 000 000 ns
     req.log.http("http_request", {
