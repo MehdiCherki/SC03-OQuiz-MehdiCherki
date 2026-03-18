0b294ef9d35c33d491aab1239a99823bb42c8f9c
BenoitOClock <benoit@oclock.io>
2026-03-18 17:09:52 +0100

    improved file controller

--- files-service/src/controllers/file.controller.ts
+++ files-service/src/controllers/file.controller.ts
@@ -1,15 +1,23 @@
 import type { Request, Response } from "express";
 import { formParser } from "../lib/upload.ts";
+import { prisma } from "../models/index.ts";
+import {
+  idParamsSchema,
+  queryParamsGetFileSchema,
+} from "../validations/utils.ts";
 
 export async function uploadFile(req: Request, res: Response): Promise<void> {
-  const [fields, files] = await formParser.parse(req);
-  /*
-  files est un objet avec des propriétés contenant le/les fichier(s) uploadés
-  ex : {file:[{...}]}
-  */
-  const filesToSave = Object.entries(files)
-    .map(([key, files]) => files || [])
-    .flat();
+  const [, files] = await formParser.parse(req);
+  const filesToSave = Object.values(files).flatMap(files => files ?? []);
 
-  res.json(filesToSave);
+  const filesCreated = await prisma.file.createManyAndReturn({
+    data: filesToSave.map(file => ({
+      size: file.size,
+      newFilename: file.newFilename,
+      originalFilename: file.originalFilename || "",
+      mimetype: file.mimetype || "",
+    })),
+  });
+
+  res.json(filesCreated);
 }
